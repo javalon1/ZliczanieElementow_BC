82a1ff12978c6b3c50146fe6b7f700fe1efbe604
Bartlomiej Cyba <bartlomiejcyba@gmail.com>
2019-05-12 14:53:46 +0100

    end

--- src/main/java/com/sda/algorytmy/App.java
+++ src/main/java/com/sda/algorytmy/App.java
@@ -2,12 +2,21 @@ package com.sda.algorytmy;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+
+        int[] a = {4, 1, 6, 1, 1, 4, 3, 3, 1};
+        int n = a.length;
+        int wynik = 0;
+        int x = 1;
+        for (int i = 0; i < n; i++) {
+
+            if (a[i] == x) {
+                wynik = wynik + 1;
+            }
+
+        }
+        System.out.println(x + " wystepuje: " + wynik + " razy ");
     }
 }
